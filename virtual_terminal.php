4b50df9196b2933e9371a320a9c38d68e9179611
Mr_ming <youminghou@gmail.com>
2018-12-02 01:00:30 -0500

    Finish up symlink

--- virtual_terminal.php
+++ virtual_terminal.php
@@ -3,7 +3,7 @@
 require "classes/fileSystem.php";
 
 $fs = new FileSystem();
-
+/*
 $fs->mkdir('usr');
 
 $fs->cd('usr');
@@ -24,16 +24,26 @@ echo $fs->pwd() . "\n";
 $fs->rmdir('/usr/orange/red');
 
 $fs->dumpFileSystem();
-/*
+*/
 $fs = new FileSystem();
+$fs->mkdir('usr');
 $fs->cd('usr');
 $fs->mkdir('local');
 $fs->cd('local');
-echo $fs->pwd();
+//echo $fs->pwd();
 $fs->cd('..');
 $fs->mkdir('share');
 $fs->mkdir('share/info');
 $fs->cd('share/info');
-echo $fs->pwd();
-*/
+//echo $fs->pwd();
+$fs->mkdir('lib');
+$fs->mkdir('lib/bin');
+$fs->cd('../../');
+$fs->symlink('share/info/lib', 'src');
+$fs->symlink('share/', 'peanut');
+$fs->cd('src/bin');
+$fs->rmdir('peanut');
+$fs->dumpFileSystem();
+echo $fs->pwd() . "\n";
+
 ?>
