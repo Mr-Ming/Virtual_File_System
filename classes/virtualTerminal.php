e927858fa59fa697ab1befaafa925dd2ff765cc8
Mr_ming <youminghou@gmail.com>
2018-12-02 22:48:56 -0500

    Add list file by given path

--- classes/virtualTerminal.php
+++ classes/virtualTerminal.php
@@ -3,18 +3,16 @@
 require "classes/fileSystem.php";
 
 class VirtualTerminal {
-
 	const INPUT_HELP = 'help';
 	const INPUT_PWD = 'pwd';
 	const INPUT_CD = 'cd';
 	const INPUT_MKDIR = 'mkdir';
 	const INPUT_RMDIR = 'rmdir';
 	const INPUT_SYMLINK = 'symli';
-	const INPUT_REMOVE_SYMLINK = 'remov';
+	const INPUT_REMOVE_SYMLINK = 'rmsym';
 	const INPUT_ADD_FILE = 'addfi';
-	const INPUT_DELETE_FILE = 'delfi';
-	const INPUT_LIST_FILE_BY_FOLDER = 'listf';
-	const INPUT_LIST_FILE_IN_CURRENT_DIRECTORY = 'list';
+	const INPUT_LIST_FILE_IN_GIVEN_PATH = 'listp';
+	const INPUT_LIST_FILE_IN_CURRENT_DIRECTORY = 'listc';
 	const INPUT_DUMP_FILE_SYSTEM = 'dump';
 	const INPUT_QUIT = 'quit';
 
@@ -23,15 +21,18 @@ class VirtualTerminal {
 	const ERROR_INVALID_SYNTAX_FOR_MKDIR = 'Invalid Syntax for `mkdir`, correct syntax is mkdir(path)';
 	const ERROR_INVALID_SYNTAX_FOR_RMDIR = 'Invalid Syntax for `rmdir`, correct syntax is rmdir(path)';
 	const ERROR_INVALID_SYNTAX_FOR_SYMLINK = 'Invalid Syntax for `symlink`, correct syntax is symlink(source, dest)';
-	const ERROR_INVALID_SYNTAX_FOR_REMOVE_SYMLINK = 'Invalid Syntax for `removeSymlink`, correct syntax is removeSymLink(link)';
+	const ERROR_INVALID_SYNTAX_FOR_REMOVE_SYMLINK = 'Invalid Syntax for `rmsym`, correct syntax is rmsym(link)';
 	const ERROR_INVALID_SYNTAX_FOR_ADD_FILE = 'Invalid Syntax for `addfile`, correct syntax is addfile(file)';
 
 	const DIRECTOR_CREATED = "Directory created %s";
 	const DIRECTOR_REMOVED = "Directory removed %s";
 	const SYMLINK_SUCCESS = "Symlink %s is successfully set";
-	const SYMLINK_REMOVED = "Symlink %s is successfully removed";
 	const CURRENT_PATH_CHANGED = "Current Path successfully changed";
 	const FILE_ADDED = "File %s successfully added";
+	const NO_FILE_FOUND_IN_CURRENT_DIRECTORY = 'No file found in current directory';
+	const COUNT_OF_FILES_FOUND_IN_CURRENT_DIRECTORY = '%s files found in current directory:';
+	const NO_FILE_FOUND_IN_GIVEN_PATH = 'No file found in given path';
+	const COUNT_OF_FILES_FOUND_IN_GIVEN_PATH = '%s files found in given path:';
 
 	private $file_system = null;
 
@@ -70,6 +71,12 @@ class VirtualTerminal {
 			case self::INPUT_ADD_FILE:
 				$this->executeAddFile($input);
 				break;
+			case self::INPUT_LIST_FILE_IN_GIVEN_PATH:
+				$this->executeListFileInGivenPath($input);
+				break;
+			case self::INPUT_LIST_FILE_IN_CURRENT_DIRECTORY:
+				$this->executeListCurrent();
+				break;
 			case self::INPUT_DUMP_FILE_SYSTEM:
 				$this->executeDumpFileSystem();
 				break;
@@ -89,8 +96,10 @@ class VirtualTerminal {
 		$this->displayMessage('rmdir(directory) : remove existing directory');
 		$this->displayMessage('cd(path) : change location to specified path');
 		$this->displayMessage('symlink(source, dest) : add symlink');
-		$this->displayMessage('removeSymlink(link) : remove symlink');
+		$this->displayMessage('rmsym(link) : remove symlink');
 		$this->displayMessage('addfile(file) : add file to current directory');
+		$this->displayMessage('listp(path) : list all files in given path');
+		$this->displayMessage('listc : list all files in current directory');
 		$this->displayMessage('dump : For debugging, dump the current file_system memory');
 		$this->displayMessage('quit : Exit virtual terminal');
 	}
@@ -182,13 +191,57 @@ class VirtualTerminal {
 	public function executeAddFile($input) {
 		$argument = $this->getArgument($input);
 		
-		var_dump($argument);
 		if(count($argument) !== 1 || $argument[0] === '') {
 			return $this->displayMessage(self::ERROR_INVALID_SYNTAX_FOR_ADD_FILE);
 		}
 
-		$this->file_system->addFile($argument[0]);
+		try {
+			$this->file_system->addFile($argument[0]);
+		} catch(Exception $e) {
+			return $this->displayMessage($e->getMessage());
+		}
+		
 		$this->displayMessage(sprintf(self::FILE_ADDED, $argument[0]));
+	}
+
+	public function executeListCurrent() {
+		$files = $this->file_system->listFilesInCurrentPath();
+		$total_files = count($files);
+		
+		if ($total_files === 0) {
+			return $this->displayMessage(self::NO_FILE_FOUND_IN_CURRENT_DIRECTORY);
+		}
+
+		$this->displayMessage(sprintf(self::COUNT_OF_FILES_FOUND_IN_CURRENT_DIRECTORY, $total_files));
+
+		foreach($files as $key=>$file) {
+			$this->displayMessage(($key+1).'-> '. $file);
+		}
+	}
+
+	public function executeListFileInGivenPath($input) {
+		$argument = $this->getArgument($input);
+		
+		if(count($argument) !== 1 || $argument[0] === '') {
+			return $this->displayMessage(self::ERROR_INVALID_SYNTAX_FOR_MKDIR);
+		}
+
+		try {
+			$files = $this->file_system->listFilesInGivenPath($argument[0]);
+			$total_files = count($files);
+		} catch(Exception $e) {
+			return $this->displayMessage($e->getMessage());
+		}
+		
+		if ($total_files === 0) {
+			return $this->displayMessage(self::NO_FILE_FOUND_IN_GIVEN_PATH);
+		}
+
+		$this->displayMessage(sprintf(self::COUNT_OF_FILES_FOUND_IN_GIVEN_PATH, $total_files));
+
+		foreach($files as $key=>$file) {
+			$this->displayMessage(($key+1).'-> '. $file);
+		}
 	}
 
 	public function executeDumpFileSystem() {
